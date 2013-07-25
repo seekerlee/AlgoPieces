e2834400c7051694413708b78d4fef229c53d762
seekerlee <lishi1608@gmail.com>
2013-07-25 11:29:47 +0800

    finish K min cut

---
+++ src/test/java/io/github/seekerlee/algopieces/KragerMinCutTest.java
@@ -0,0 +1,11 @@
+package io.github.seekerlee.algopieces;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sshli
+ * Date: 7/25/13
+ * Time: 10:03 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class KragerMinCutTest {
+}
